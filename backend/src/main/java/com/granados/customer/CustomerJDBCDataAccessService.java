5c35218ddfb5f55b840aebd72090857bb470204e
Sandro Granados <dimesandro@gmail.com>
2023-09-17 20:00:28 -0500

    break existsCustomerWithEmail test method

--- backend/src/main/java/com/granados/customer/CustomerJDBCDataAccessService.java
+++ backend/src/main/java/com/granados/customer/CustomerJDBCDataAccessService.java
@@ -60,7 +60,7 @@ public class CustomerJDBCDataAccessService implements CustomerDao {
         var sql = """
                 SELECT count(id)
                 FROM customer
-                WHERE email = ?
+                WHERE name = ?
                 """;
 
         Integer count = jdbcTemplate.queryForObject(sql, Integer.class, email);
