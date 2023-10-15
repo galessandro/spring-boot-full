025df4fd93bde8ff51e8cafe7235904bfc6d831a
Sandro Granados <dimesandro@gmail.com>
2023-10-15 14:27:55 -0500

    Add/Remove customer feature

--- frontend/react/src/components/Card.jsx
+++ frontend/react/src/components/Card.jsx
@@ -9,10 +9,45 @@ import {
   Stack,
   useColorModeValue,
   Tag,
+  Button,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  useDisclosure,
 } from '@chakra-ui/react'
+import { deleteCustomer } from '../services/client';
+import { errorNotification, successNotification } from '../services/notification';
+import { useRef } from 'react';
 
-export default function CardWithImage({id, name, email, age, gender, imageNumber}) {
+export default function CardWithImage({id, name, email, age, gender, imageNumber, fetchCustomers}) {
   const randomUserGender = gender === "MALE" ? "men" : "women";
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = useRef()
+
+  const removeCustomer = () => {
+    deleteCustomer(id).then(res => {
+      console.log(res)
+      successNotification(
+        "Customer deleted",
+        `${name} was successfully deleted`
+      );
+      fetchCustomers();
+    })
+    .catch(err => {
+      console.log(err);
+      errorNotification(
+        err.code,
+        err?.response.data.message
+      )
+    })
+    .finally(() => {
+      onClose()
+    })
+  }
+
   return (
     <Center py={6}>
       <Box
@@ -51,6 +86,49 @@ export default function CardWithImage({id, name, email, age, gender, imageNumber
             </Heading>
             <Text color={'gray.500'}>{ email }</Text>
             <Text color={'gray.500'}>Age { age } | { gender }</Text>
+          </Stack>
+          <Stack m={8}>
+            <Button
+              mt={8}
+              bg={'red.400'}
+              color='white'
+              rounded={'full'}
+              _hover={{
+                transform: 'translateY(-2px)',
+                boxShadow: 'lg'
+              }}
+              _focus={{
+                bg: 'grey.500'
+              }}
+              onClick={onOpen}>
+              Delete
+            </Button>
+            <AlertDialog
+              isOpen={isOpen}
+              leastDestructiveRef={cancelRef}
+              onClose={onClose}
+            >
+              <AlertDialogOverlay>
+                <AlertDialogContent>
+                  <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                    Delete Customer
+                  </AlertDialogHeader>
+
+                  <AlertDialogBody>
+                    Are you sure you want do delete {name}? You can't undo this action afterwards.
+                  </AlertDialogBody>
+
+                  <AlertDialogFooter>
+                    <Button ref={cancelRef} onClick={onClose}>
+                      Cancel
+                    </Button>
+                    <Button colorScheme='red' onClick={removeCustomer} ml={3}>
+                      Delete
+                    </Button>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialogOverlay>
+            </AlertDialog>
           </Stack>
         </Box>
       </Box>
